556be78b28c6f6cd87d16b3b9e45364617a0c91d
mirocsd <mirostrasz@gmail.com>
2024-01-03 20:13:04 -0700

    Added acutal queue functionality, changed variable names to reflect this. Removed 'Add X' buttons, and included a tooltip to tell the user to press Enter/Return to add the thing to the queue. Changed QT5 GUI (still ugly)

--- queuer.py
+++ queuer.py
@@ -1,51 +1,67 @@
 import sys
-from PyQt5 import QtWidgets, uic
+from PyQt5 import QtWidgets, uic, QtCore
 
 class MyWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(MyWindow, self).__init__()
         uic.loadUi('./myui.ui', self)
 
-        self.addAlbumButton.clicked.connect(self.add_album)
-        self.addBookButton.clicked.connect(self.add_book)
-        self.addMovieButton.clicked.connect(self.add_movie)
+        self.editAlbum.installEventFilter(self)
+        self.editBook.installEventFilter(self)
+        self.editMovie.installEventFilter(self)
 
         self.removeAlbumButton.clicked.connect(self.remove_album)
         self.removeBookButton.clicked.connect(self.remove_book)
         self.removeMovieButton.clicked.connect(self.remove_movie) 
         
+    # This is an 'event filter' for detecting when the enter key is pressed,
+    # so we know when to add to which list.    
+    def eventFilter(self, input_source, event):
+        if event.type() == QtCore.QEvent.KeyPress and event.key() == QtCore.Qt.Key_Return:
+            if input_source == self.editAlbum:
+                self.add_album()
+            elif input_source == self.editBook:
+                self.add_book()
+            elif input_source == self.editMovie:
+                self.add_movie()
+            return True
+        return False
 
+    # Functions to add X to the 'X' queue
     def add_album(self):
         album_name = self.editAlbum.text()
         if album_name:
-            self.listAlbums.addItem(album_name)
-            self.musicNameEdit.clear()
+            self.albumQueue.addItem(album_name)
+            self.editAlbum.clear()
 
     def add_book(self):
         book_name = self.bookNameEdit.text()
         if book_name:
-            self.listBooks.addItem(book_name)
-            self.bookNameEdit.clear()
+            self.bookQueue.addItem(book_name)
+            self.editBook.clear()
 
     def add_movie(self):
         item_name = self.movieNameEdit.text()
         if item_name:
-            self.listMovies.addItem(item_name)
-            self.movieNameEdit.clear()
+            self.movieQueue.addItem(item_name)
+            self.editMovie.clear()
 
+
+    # Specific functions to remove the last X added
     def remove_album(self):
-        self.remove_selected_item(self.listAlbum)
+        self.remove_last(self.albumQueue)
 
     def remove_book(self):
-        self.remove_selected_item(self.listBooks)
+        self.remove_last(self.bookQueue)
 
     def remove_movie(self):
-        self.remove_selected_item(self.listMovies)
+        self.remove_last(self.movieQueue)
 
-    def remove_selected_item(self, list_widget):
-        selected_item = list_widget.currentRow()
-        if selected_item >= 0:
-            list_widget.takeItem(selected_item)
+
+    # General function to remove the first (chronologically) X added
+    def remove_last(self, list_widget):
+        if list_widget.count() > 0:
+            list_widget.takeItem(0)
 
 
 if __name__ == '__main__':
